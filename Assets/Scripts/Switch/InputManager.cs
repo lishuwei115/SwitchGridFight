4acfcc45b14982acbe46ed078e4945966304c79a
DESKTOP-OLPNEG9\hello <dev@playtra.com>
2019-08-07 14:48:57 +0100

    Riki - rewired

--- Assets/Scripts/Switch/InputManager.cs
+++ Assets/Scripts/Switch/InputManager.cs
@@ -1,11 +1,73 @@
-﻿using System.Collections;
+﻿using Rewired;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public int playerId = 0;
+
+    // The movement speed of this character
+    public float moveSpeed = 3.0f;
+
+    // The bullet speed
+    public float bulletSpeed = 15.0f;
+
+    // Assign a prefab to this in the inspector.
+    // The prefab must have a Rigidbody component on it in order to work.
+    public GameObject bulletPrefab;
+
+    private Player player; // The Rewired Player
+    private CharacterController cc;
+    private Vector3 moveVector;
+    private bool fire;
+
+    void Awake()
+    {
+        // Get the Rewired Player object for this player and keep it for the duration of the character's lifetime
+        player = ReInput.players.GetPlayer(playerId);
+
+        // Get the character controller
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        GetInput();
+        ProcessInput();
+    }
+
+    private void GetInput()
+    {
+        // Get the input from the Rewired Player. All controllers that the Player owns will contribute, so it doesn't matter
+        // whether the input is coming from a joystick, the keyboard, mouse, or a custom controller.
+
+       // moveVector.x = player.GetAxis("Move Horizontal"); // get input by name or action id
+       // moveVector.y = player.GetAxis("Move Vertical");
+        fire = player.GetButtonDown("LLeftArrow");
+    }
+
+    private void ProcessInput()
+    {
+        // Process movement
+        if (moveVector.x != 0.0f || moveVector.y != 0.0f)
+        {
+            cc.Move(moveVector * moveSpeed * Time.deltaTime);
+        }
+
+        // Process fire
+        if (fire)
+        {
+            Debug.Log("Enter");
+        }
+    }
+}
+
+/*
+
+// Start is called before the first frame update
+void Start()
     {
         
     }
@@ -77,3 +139,4 @@ public class InputManager : MonoBehaviour
         }
     }
 }
+*/
